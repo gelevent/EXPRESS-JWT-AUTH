50724b8d4132831c02424b83f84f0767ff2b2df9
gelevent <gemiwidodosmktibazma@gmail.com>
2024-05-10 14:12:33 +0700

    first commit

---
+++ Config/Connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool( {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: 3306,
+    waitForConnections: false,
+    connectionLimit: 10,
+    queueLimit: 0,
+    database: 'bazma_jwt_auth',
+});
+
+module.exports = connection;
