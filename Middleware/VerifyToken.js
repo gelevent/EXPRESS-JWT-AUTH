491486bf295d28c91cfc05fa2a9d55e0f9cf3060
gelevent <gemiwidodosmktibazma@gmail.com>
2024-05-15 13:41:22 +0700

    Todos dan logout

--- Middleware/VerifyToken.js
+++ Middleware/VerifyToken.js
@@ -1,25 +1,16 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
-const verifyToken = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader) {
-    return res.status(401).json({
-      message: "No token provided. Unauthorized",
-    });
-  }
-
-  const token = authHeader.split(" ")[1];
-
-  jwt.verify(token, "bazmaSecretKey", (err, user) => {
-    if (err) {
-      return res.status(403).json({
-        message: "Invalid token. Forbidden",
-      });
-    }
-    req.user = user;
-    next();
-  });
-};
+async function verifyToken(req, res, next) {
+    const token = req.headers.authorization;
+    jwt.verify(token, 'bazmaSecretKey', function (err, decoded) {
+        if (err) {
+            return res.status(401).json({
+                message: "Invalid Token, Unauthorized"
+            })
+        }
+        req.user = decoded;
+        return next();
+    })
+}
 
 module.exports = verifyToken;
--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors')
 const app = express();
 
-app.use(cors());
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const authRoute = require('./Routes/AuthRoute');
-app.use('/api/auth', authRoute);
 
-const PORT = process.env.PORT || 3000;
+const AuthRoute = require('./Routes/AuthRoute');
+const TodoRoute = require('./Routes/TodoRoute');
+app.use('/api/auth', AuthRoute);
+app.use('/api', TodoRoute);
+
+const PORT = 3000 || process.env.PORT;
 
 app.listen(PORT, () => {
-    // Menjalankan PORT
+    // menjalankan PORT
     console.log(`Server is running on port ${PORT}`);
 });
